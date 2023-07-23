38f630bf24fa8da5641419bc6b8019a336c28251
Linga Granites <lingagranites@gmail.com>
2023-07-23 12:27:13 +0530

    23-17 commit

--- server/server.js
+++ server/server.js
@@ -4,12 +4,23 @@ const cors = require('cors');
 const app = express();
 const User= require('./model/dataSchema');
 const path = require('path');
+
 const dotenv = require('dotenv').config({path :'config.env'});
 
 app.use(express.json());
 app.use(cors());
 
-app.post('/', async(req, res) => {
+// mongoose.connect('mongodb+srv://lingagranites:lingadevi%402017@lingagranites.padzhqc.mongodb.net/lingagranites', { useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.DB_URI)
+.then(()=>console.log('Connected to Database'))
+.catch((er)=>console.log("connection error"+" => "+er));
+
+
+// app.get('/',(req,res)=>{
+//     res.status(200).send('server side started')
+// })
+
+app.post('/insert', async(req, res) => {
     const name = req.body.name;
     const number = req.body.number;
     const email = req.body.email;
@@ -31,20 +42,29 @@ app.post('/', async(req, res) => {
     }
 });
 
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join(__dirname, '../client/build')));
-    app.get("*/",(req,res)=>{
-        res.sendFile(path.resolve(__dirname,"../client/build/index.html"));
-    });
-}
-
-mongoose.connect('mongodb+srv://lingagranites:lingadevi%402017@lingagranites.padzhqc.mongodb.net/lingagranites', { useNewUrlParser: true, useUnifiedTopology: true})
-// mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true,useCreateIndex: true,})
-.then(()=>console.log('Connected to Database'))
-.catch((er)=>console.log("connection error"+" => "+er));
+// if(process.env.NODE_ENV === "production"){
+//     app.use(express.static(path.join(__dirname, '../client/build')));
+//     app.get("*/",(req,res)=>{
+//         res.sendFile(path.resolve(__dirname,"../client/build/index.html"));
+//     });
+// }
 
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
 })
+
+function serveReactApp(req, res, next) {
+    const clientAppPath = path.join(__dirname, '../client/build');
+    const indexPath = path.resolve(__dirname, '../client/build/index.html');
+  
+    express.static(clientAppPath)(req, res, (err) => {
+      if (err) {
+        res.sendFile(indexPath);
+      }
+    });
+  }
+
+  app.use(serveReactApp);
+  
